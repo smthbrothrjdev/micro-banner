9d0de9db09c20cb59f5eee914bda000f344b08ef
BJ Jackson <bj.jackson.mil@swf.army.mil>
2024-03-01 16:40:46 -0600

    v1.0.0

--- src/main/java/org/jackson/banner/MotdController.java
+++ src/main/java/org/jackson/banner/MotdController.java
@@ -1,16 +1,18 @@
 package org.jackson.banner;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin()
 @RestController
 public class MotdController {
 
   @GetMapping("/api/motd")
-  public String messageOfTheDay(){
-    return "there are no alerts";
+  public ResponseEntity messageOfTheDay(){
+
+    return new ResponseEntity<>("Streaming this in", HttpStatus.OK);
   }
 
 }
